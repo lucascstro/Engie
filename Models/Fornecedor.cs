7108dfdc67c49c1f9ad1ef45d995671bece81ad7
lucascstro <lukas.castro@live.com>
2021-08-02 01:18:02 -0300

    Criado context, models e entidades

---
+++ Models/Fornecedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Engie.Models
+{
+    public class Fornecedor
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Fornecedor")]
+        [Required]
+        public string Nome { get; set; }
+    }
+}
---
+++ Models/Usina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Engie.Models
+{
+    public class Usina
+    {
+        public int Id { get; set; }
+        [Required]
+        public string  Uc { get; set; }
+        [Required]
+        public bool  Ativo { get; set; }            
+        public IList<Fornecedor> Fornecedor { get; set; }
+    }
+}
